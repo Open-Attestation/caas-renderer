06c4e419c2b536635f76af4e6bdd48c107d8f6c2
Sim Boon Long <simboonlong@gmail.com>
2019-12-27 17:35:21 +0800

    use styled approach refactor divs into components

--- src/templates/customTemplate/customTemplate.tsx
+++ src/templates/customTemplate/customTemplate.tsx
@@ -1,77 +1,140 @@
 import React, { FunctionComponent, useState } from "react";
 import { TemplateProps } from "@govtechsg/decentralized-renderer-react-components";
 import { format } from "date-fns";
-import { css } from "@emotion/core";
+// import { css } from "@emotion/core";
+import styled from "@emotion/styled";
 import { CustomTemplateCertificate, Rating } from "../sample";
 
 const colorWhite = "#ffffff";
+// const colorMaroon = "#b1344b";
 const colorBlue = "#3463b1";
 const colorBlueDark = "#2a4e8A";
 const easeOutCubic = "cubic-bezier(0.215, 0.61, 0.355, 1)";
 
-const borderLeft = css(`
+const cardW = "250px";
+const cardH = "400px";
+
+const FieldGroup = styled.div`
   border: 0;
   border-left: 2px solid ${colorWhite};
-`);
+  padding-left: ${props => (props.front ? "15px" : "5px")};
+  margin-bottom: ${props => (props.front ? "10px" : "5px")};
+  flex: ${props => props.flex};
+`;
 
-const fieldGroupStyle = css(`
-  padding-left: 15px;
-  margin-bottom: 10px;
-`);
-
-const labelStyle = css(`
+const RatingGroup = styled.div`
   font-size: 0.6em;
-`);
+  margin-bottom: 10px;
 
-const valueStyle = css(`
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const RatingName = styled.div`
+  font-weight: 900;
+  margin-bottom: 5px;
+`;
+
+const RatingHeading = styled.div`
   font-size: 0.8em;
   font-weight: 900;
-`);
+  margin-bottom: 20px;
+`;
 
-const backFieldGroupStyle = css(`
-  padding-left: 5px;
-  margin-bottom: 5px;
-`);
+const LabelText = styled.div`
+  font-size: 0.6em;
+`;
 
-const scene = css(`
+const LabelValue = styled.div`
+  font-size: 0.8em;
+  font-weight: 900;
+`;
+
+const Profile = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 50%;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+const ProfileContent = styled.div`
+  display: flex;
+`;
+
+const ProfileAgency = styled.div`
+  flex: 1 1 0%;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const ProfileShot = styled.div`
+  flex: 1 1 0%;
+  padding: 20px;
+  background-color: ${colorWhite};
+`;
+
+const LicenseDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 50%;
+  padding-left: 20px;
+  padding-right: 20px;
+  background-color: ${colorBlueDark};
+`;
+
+const LicenseInfo = styled.div`
+  display: flex;
+  padding: 20px;
+  font-size: 0.8em;
+  text-align: center;
+`;
+
+const LicenseRatings = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 50%;
+  padding-left: 20px;
+  padding-right: 20px;
+  background-color: ${colorBlueDark};
+`;
+
+const Scene = styled.div`
   position: absolute;
   left: 50%;
   top: 50%;
-  transform: translate(-50% ,-50%);
+  transform: translate(-50%, -50%);
   perspective: 1000;
-`);
+`;
 
-const flipCard = css(`
+const FlipCard = styled.div<{ isFlipped: boolean }>`
+  transform: rotateY(${({ isFlipped }) => (isFlipped ? "-180deg" : "0deg")});
   position: relative;
   transition: transform 0.7s ${easeOutCubic};
   transform-style: preserve-3d;
-  width: 250px;
-  height: 400px;
+  width: ${cardW};
+  height: ${cardH};
   margin-left: auto;
   margin-right: auto;
   color: ${colorWhite};
-`);
+`;
 
-const flipped = css(`
-  transform: rotateY(180deg);
-`);
-
-const flipCardFace = css(`
+const FlipCardFace = styled.div<{ isFront: boolean }>`
+  transform: rotateY(${({ isFront }) => (isFront ? "0deg" : "-180deg")});
   backface-visibility: hidden;
   position: absolute;
   height: 100%;
   width: 100%;
   background-color: ${colorBlue};
   border-radius: 15px;
-`);
-
-const flipCardFaceFront = css(`
-  transform: rotateY(0deg);
-`);
-
-const flipCardFaceBack = css(`
-  transform: rotateY(180deg);
-`);
+  overflow: hidden;
+`;
 
 interface RatingsProps {
   ratings: Rating[];
@@ -79,19 +142,19 @@ interface RatingsProps {
 
 const Ratings: FunctionComponent<RatingsProps> = ({ ratings }: { ratings: Rating[] }) => {
   const rendererdRatings = ratings.map((rating, index) => (
-    <div css={{ marginBottom: 10, fontSize: "0.6em" }} key={index}>
-      <div css={{ fontWeight: 900, marginBottom: 5 }}>{rating.name}</div>
+    <RatingGroup key={index}>
+      <RatingName>{rating.name}</RatingName>
       <div className="d-flex">
-        <div css={[{ flex: 2 }, backFieldGroupStyle, borderLeft]}>
+        <FieldGroup flex="2">
           <div>Limitations:</div>
           <div>{rating.limitations || "NIL"}</div>
-        </div>
-        <div css={[{ flex: 1 }, backFieldGroupStyle, borderLeft]}>
+        </FieldGroup>
+        <FieldGroup flex="1">
           <div>Valid Till:</div>
           <div>{format(new Date(rating.expiryDate), "dd MMM yyyy")}</div>
-        </div>
+        </FieldGroup>
       </div>
-    </div>
+    </RatingGroup>
   ));
   return <>{rendererdRatings}</>;
 };
@@ -108,60 +171,48 @@ export const CustomTemplate: FunctionComponent<TemplateProps<CustomTemplateCerti
   };
 
   return (
-    <div css={scene}>
-      <div css={isFlipped ? [flipCard, flipped] : [flipCard]} onClick={onClickFlipCard}>
-        <div css={[flipCardFace, flipCardFaceFront]}>
-          <div css={{ height: 200, paddingTop: 20 }} className="d-flex">
-            <div css={{ flex: 1, padding: 20 }} className="d-flex align-items-center">
-              <img src="https://www.caas.gov.sg/assets/caas/images/logo-caas-white.png" className="w-100" />
-            </div>
-            <div css={{ flex: 1, padding: 20, background: "white", margin: 20 }}>
-              <img src={document.recipient.photo} className="w-100 h-100" />
-            </div>
-          </div>
-          <div
-            className="flex-fill"
-            css={{
-              backgroundColor: colorBlueDark,
-              borderBottomRightRadius: 15,
-              borderBottomLeftRadius: 15,
-              padding: 20
-            }}
-          >
-            <div css={[fieldGroupStyle, borderLeft]}>
-              <div css={labelStyle}>License Number:</div>
-              <div css={valueStyle}>{document.id}</div>
-            </div>
-            <div css={[fieldGroupStyle, borderLeft]}>
-              <div css={labelStyle}>Name of Pilot:</div>
-              <div css={valueStyle}>{document.recipient.name}</div>
-            </div>
-            <div css={[fieldGroupStyle, borderLeft]}>
-              <div css={labelStyle}>Birth Date:</div>
-              <div css={valueStyle}>{format(new Date(document.recipient.dob), "dd MMM yyyy")}</div>
-            </div>
-            <div css={[fieldGroupStyle, borderLeft]}>
-              <div css={labelStyle}>Issue Date:</div>
-              <div css={valueStyle}>{format(new Date(document.issuanceDate), "dd MMM yyyy")}</div>
-            </div>
-          </div>
-        </div>
-        <div css={[flipCardFace, flipCardFaceBack]}>
-          <div css={{ padding: 20, fontSize: "0.8em", textAlign: "center" }} className="d-flex">
+    <Scene>
+      <FlipCard isFlipped={isFlipped} onClick={onClickFlipCard}>
+        <FlipCardFace isFront={true}>
+          <Profile>
+            <ProfileContent>
+              <ProfileAgency>
+                <img className="img-fluid" src="https://www.caas.gov.sg/assets/caas/images/logo-caas-white.png" />
+              </ProfileAgency>
+              <ProfileShot>
+                <img className="img-fluid" src={document.recipient.photo} />
+              </ProfileShot>
+            </ProfileContent>
+          </Profile>
+          <LicenseDetails>
+            <FieldGroup front>
+              <LabelText>License Number:</LabelText>
+              <LabelValue>{document.id}</LabelValue>
+            </FieldGroup>
+            <FieldGroup front>
+              <LabelText>Name of Pilot:</LabelText>
+              <LabelValue>{document.recipient.name}</LabelValue>
+            </FieldGroup>
+            <FieldGroup front>
+              <LabelText>Birth Date:</LabelText>
+              <LabelValue>{format(new Date(document.recipient.dob), "dd MMM yyyy")}</LabelValue>
+            </FieldGroup>
+            <FieldGroup front>
+              <LabelText>Issue Date:</LabelText>
+              <LabelValue>{format(new Date(document.issuanceDate), "dd MMM yyyy")}</LabelValue>
+            </FieldGroup>
+          </LicenseDetails>
+        </FlipCardFace>
+        <FlipCardFace isFront={false}>
+          <LicenseInfo>
             THIS PILOT IS ENTITLED TO EXERCISE THE PRIVILEGES IN CONJUNCTION WITH THE RATINGS SHOWING BELOW:
-          </div>
-          <div
-            className="flex-fill"
-            css={{
-              backgroundColor: colorBlueDark,
-              padding: 20
-            }}
-          >
-            <div css={{ fontWeight: 900, fontSize: "0.8em", marginBottom: 20 }}>UNMANNED AIRCRAFT RATING</div>
+          </LicenseInfo>
+          <LicenseRatings>
+            <RatingHeading>UNMANNED AIRCRAFT RATING</RatingHeading>
             <Ratings ratings={document.ratings} />
-          </div>
-        </div>
-      </div>
-    </div>
+          </LicenseRatings>
+        </FlipCardFace>
+      </FlipCard>
+    </Scene>
   );
 };
